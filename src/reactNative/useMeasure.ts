380bae571cae4fe759cd9090259b9508fa1a6d39
Don Townsend <don@teamairship.com>
2019-11-01 12:41:57 -0400

    tweak useMeasure && add usage notes

--- src/reactNative/useMeasure.ts
+++ src/reactNative/useMeasure.ts
@@ -2,19 +2,40 @@ import { useRef } from "react";
 
 import measureNode from "./utils/position/measureNode";
 
-interface Measure {
-  ref: {
-    current?: React.ReactNode;
-  };
-  measure: Function;
-}
+type Measure = [{ current?: React.ReactNode }, Function];
 
-export default function useMeasure(
-  _key: string,
-  _keyNext: string,
-  placement: string
-): Measure {
+/**
+ * This hook provides you with a `ref` and a `measure` useful
+ * for gathering positional and dimensional data from a RN node.
+ *
+ * TO-DO: add `callback` param to `useMeasure` that will replace
+ * the `useEffect` implementation below and simplify usage.
+ *
+ * **USAGE:**
+ *
+ * ```
+ * const MyComponent = () => {
+ *   const [ref, measure] = useMeasure();
+ *
+ *   useEffect(() => {
+ *     const calcPosition = async () => {
+ *       const { x, y, width, height } = await measure();
+ *       // do something with this data...
+ *     };
+ *     // adding this setTimeout made this work for me, I think
+ *     // because it guarantees that the render happens before
+ *     // the position is calc'd
+ *     setTimeout(calcPosition, 0);
+ *   }, [ref.current]);
+ *
+ *   return (
+ *     <SomeComponent ref={ref} />
+ *   )
+ * }
+ * ```
+ */
+export default function useMeasure(): Measure {
   const ref = useRef(null);
   const measure = async () => measureNode(ref.current);
-  return { ref, measure };
+  return [ref, measure];
 }
