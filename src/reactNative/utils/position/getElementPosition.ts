7413c975da2151130a3179719540846671d69fe8
Don Townsend <don@teamairship.com>
2019-11-01 11:56:43 -0400

    add reactNative dir and getElementPosition util

---
+++ src/reactNative/utils/position/getElementPosition.ts
@@ -0,0 +1,49 @@
+type numOrNull = number | null | undefined;
+
+export interface Coords {
+  x: numOrNull;
+  y: numOrNull;
+  width: numOrNull;
+  height: numOrNull;
+}
+
+interface ReactNativeNode {
+  measure?: Function;
+}
+
+/**
+ * Given a react native node, measure and return its position and sizing
+ * @param {ReactChild} node
+ * @returns {Promise<Coords>}
+ */
+export default async function measurePosition(
+  node: React.ReactChild & ReactNativeNode
+): Promise<Coords> {
+  const defaultVal: Coords = {
+    x: null,
+    y: null,
+    width: null,
+    height: null
+  };
+  if (!node || typeof node.measure !== "function")
+    return Promise.resolve(defaultVal);
+  return new Promise(resolve => {
+    node.measure(
+      (
+        _fx: numOrNull,
+        _fy: numOrNull,
+        width: numOrNull,
+        height: numOrNull,
+        px: numOrNull,
+        py: numOrNull
+      ) => {
+        resolve({
+          x: px,
+          y: py,
+          width,
+          height
+        });
+      }
+    );
+  });
+}
